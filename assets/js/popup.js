25eeec10805e715dbacbcea38d40efc8dcccb6cc
Seol hwa <devseolhwa@gmail.com>
2023-03-09 10:07:43 +0900

    Initial commit

---
+++ assets/js/popup.js
@@ -0,0 +1,131 @@
+// modar_popup
+$(function () {
+	$(".modarPopup.show").each(function () {
+		modarOpen($(this));
+	});
+});
+
+var modarOpener = null;
+$(document).on("click", ".js_modar_close , .btn_close_modar", function(e) {
+    var target = $(this).closest(".modarPopup").attr("id");
+    modarClose("#" + target, modarOpener);
+});
+
+function modarOpen(_target) {
+    let modar_body = $(_target).children().children().children(".modar_body");
+    let firstPopupBox = $(_target).children().children().children().children(".firstPopupBox");
+    let firstPopupBoxIs = $(modar_body).next().is(".firstPopupBox");
+    console.log(firstPopupBox);
+    console.log(firstPopupBoxIs);
+
+    if (firstPopupBoxIs) {
+        $(_target).addClass("firstPopupBoxOpen");
+        setTimeout(function() { 
+            $(_target).fadeIn("fast").addClass("show");
+            $(_target).attr("tabindex", "0").focus();
+            bodyScroll(true, $("body").width());
+        }, 100);
+        setTimeout(function() { 
+            $(_target).removeClass("firstPopupBoxOpen");
+        }, 3000);
+    } else {
+        $(_target).fadeIn("fast").addClass("show");
+        $(_target).attr("tabindex", "0").focus();
+        bodyScroll(true, $("body").width());
+    }
+}
+
+function modarClose(_target, _opener) {
+    bodyScroll(false);
+    var tg = null;
+
+    if (_opener) {
+        tg = $(_target);
+        modarOpener = $(_opener);
+    } else {
+        tg = $(".modarPopup.show");
+        modarOpener = null;
+    }
+
+    $(tg).removeClass("show");
+    if (modarOpener !== null) {
+        modarOpener.focus();
+        modarOpener = null;
+    }
+}
+
+function bodyScroll(_status, _orgWidth) {
+    var $fixedObj = $("body");
+    if (_status) {
+        $("body").addClass("modar_opened");
+        if ($("html").get(0).scrollWidth > $("html").width() === false) {
+            $fixedObj.css("margin-right", $("body").width() - _orgWidth);
+        }
+    } else {
+        $fixedObj.css("margin-right", "");
+        $("body").removeClass("modar_opened");
+    }
+}
+
+$(function () {
+
+    // popup tab
+    $(".tab_btn_group .on a").each(function () {
+        var opt = $(this).attr("href");
+        if (opt !== "#" && opt !== "#;" && opt.charAt(0) === "#") {
+            $(opt + ".tab_detail").css("display", "block");
+        }
+    });
+
+    $(".tab_btn_group a").on("click", function (e) {
+        var opt = $(this).attr("href");
+        $(this).parent("li").addClass("on").siblings("li").removeClass("on");
+
+        if (opt === "#" || opt === "" || opt === "#;") {
+            e.preventDefault();
+        } else if (opt.charAt(0) === "#") {
+            if ($(opt).hasClass("tab_detail")) {
+                $(opt).show().siblings(".tab_detail").hide();
+                e.preventDefault();
+            }
+        }
+    });
+
+    // 마일리지 쿠폰 선택 on 
+    $(".MileageCouponBtn li").click(function(e){
+        e.preventDefault();
+        $(this).addClass("on").siblings("li").removeClass("on");
+        $(this).parent().next().next(".js_modar_close").removeClass("disabled");
+    });
+});
+
+// 실물경품 slide
+let options = {};
+if ($(".realitemSlide .slideItem").length == 1) {
+    options = {
+        loop: false,
+        autoplay: false
+    }
+    $(".realitemSlide .navigation").hide();
+} else {
+    options = {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        centeredSlides: true,
+        loop: false,
+        speed: 800,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+        }, 
+        navigation: {
+            prevEl: ".swiper-button-prev",
+            nextEl: ".swiper-button-next"
+        },
+        pagination: {
+            el: ".pagination",
+            clickable: true
+        }
+    }
+}
+var realitemSlide = new Swiper(".realitemSlide", options);
---
+++ assets/js/ui.js
@@ -0,0 +1,179 @@
+$(document).ready(function(){
+
+    skrollr.init();
+    setTimeout(function(){
+        svgEffect();
+        textEffect();
+    }, 1000);
+    $(window).scroll(function(){
+        svgEffect();
+    });
+
+    function svgEffect(){
+        $(".svgEffect").each(function(){
+            let $this = $(this);
+            let start_pos = "top bottom";
+            let end_pos =  "bottom top";
+
+            ScrollTrigger.create({
+                trigger: $this,
+                start: start_pos, 
+                end: end_pos,
+                onEnter: function(){
+                    $this.addClass("active");
+                },onLeave: function(){
+                    $this.removeClass("active");
+                },onEnterBack: function(){
+                    $this.addClass("active");
+                },onLeaveBack: function(){
+                    $this.removeClass("active");
+                }
+            });
+        });
+    }
+
+    function textEffect(){
+        $(".textEffect").each(function(){
+            let $this = $(this);
+            let start_pos = "top bottom";
+            let end_pos =  "bottom top";
+
+            ScrollTrigger.create({
+                trigger: $this,
+                start: start_pos, 
+                end: end_pos,
+                onEnter: function(){
+                    $this.addClass("active");
+                },onLeave: function(){
+                    $this.removeClass("active");
+                },onEnterBack: function(){
+                    $this.addClass("active");
+                },onLeaveBack: function(){
+                    $this.removeClass("active");
+                }
+            });
+        });
+    }
+
+    // floating 스크롤후 나타나기
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $(".floating").fadeIn("slow");
+        }else {
+            $(".floating").fadeOut("slow");
+        }
+    });     
+
+    // 상품전체보기
+    $("#btnTable").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass("active");
+
+        let openCheck = $(this).is(".active");
+        if (openCheck) {
+            $(this).parents().next(".tableData").addClass("active");
+            $(this).parents().next(".tableData").slideDown(500);
+        }else {
+            $(this).parents().next(".tableData").removeClass("active");
+            $(this).parents().next(".tableData").slideUp(500);
+        }
+    });
+
+    // 무결점 출석상자 이동
+    $(".btnQuestion").click(function(e){         
+        e.preventDefault();
+        $("html, body").animate({scrollTop:$(this.hash).offset().top}, 1000);
+    });
+
+    // 무결점 출석상자 slide
+    var slideBox = new Swiper(".slideBox",{
+        slidesPerView: "auto",
+        spaceBetween: 0,
+        centeredSlides: true,
+        loop: true,
+        speed: 800,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".pagination",
+            clickable: true,
+        }
+    });
+
+    // 스냅스크롤
+    const sections = document.querySelectorAll(".section");
+    const scrolling = {
+        enabled: true,
+        events: "scroll, wheel, touchmove, pointermove".split(","),
+        prevent: e => e.preventDefault(),
+        disable() {
+        if (scrolling.enabled) {
+            scrolling.enabled = false;
+            window.addEventListener("scroll", gsap.ticker.tick, {passive: true});
+            scrolling.events.forEach((e, i) => (i ? document : window).addEventListener(e, scrolling.prevent, {passive: false}));
+        }
+        },
+        enable() {
+        if (!scrolling.enabled) {
+            scrolling.enabled = true;
+            window.removeEventListener("scroll", gsap.ticker.tick);
+            scrolling.events.forEach((e, i) => (i ? document : window).removeEventListener(e, scrolling.prevent));
+        }
+        }
+    };
+    function goToSection(section, anim, i) {
+        if (scrolling.enabled) {
+            scrolling.disable();
+            gsap.to(window, {
+                scrollTo: {y: section, autoKill: false},
+                onComplete: scrolling.enable,
+                duration: 1
+            });
+
+            anim && anim.restart();
+        }
+    }
+    sections.forEach((section, i) => {
+        ScrollTrigger.create({
+            trigger: section,
+            start: "top bottom-=1",
+            end: "bottom top+=1",
+            //onEnter: () => goToSection(section, intoAnim),
+            onEnter: () => goToSection(section),
+            onEnterBack: () => goToSection(section)
+        });
+    });
+
+    // VAL타 강사
+    $(".teacherTab li.on a").each(function () {
+        var opt = $(this).attr("href");
+        if (opt !== "#" && opt !== "#;" && opt.charAt(0) === "#") {
+            $(opt + ".teacherDetail").css("display", "block");
+        }
+    });
+
+    $(".teacherTab li a").on("click", function (e) {
+        var opt = $(this).attr("href");
+        $(this).parent("li").addClass("on").siblings("li").removeClass("on");
+
+        if ($(this).parent("li").hasClass("doenmonav")) {
+            $(".valtaTeacher").addClass("doenmoTeacher");
+            $(".valtaTeacher").removeClass("bunnyTeacher");
+        } else if ($(this).parent("li").hasClass("bunnynav")) {
+            $(".valtaTeacher").addClass("bunnyTeacher");
+            $(".valtaTeacher").removeClass("doenmoTeacher");
+        }
+
+        if (opt === "#" || opt === "" || opt === "#;") {
+            e.preventDefault();
+        } else if (opt.charAt(0) === "#") {
+            if ($(opt).hasClass("teacherDetail")) {
+                $(opt).show().siblings(".teacherDetail").hide();
+                e.preventDefault();
+            }
+        }
+    });
+
+});
