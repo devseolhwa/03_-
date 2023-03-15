e950fc3db65caa719e45d48aa9c7ce8cbf5bbfe8
Seol hwa <devseolhwa@gmail.com>
2023-03-15 11:24:12 +0900

    fdsf

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -18,6 +18,9 @@ $(document).ready(function(){
             $("body").addClass("mobileFilter");
             $(".videoItem").addClass("active");
             $(".floating").show();
+            document.querySelector(".btnGet").addEventListener("touchstart", function(e) {
+                e.preventDefault();
+            });
         }else{
             //alert("PC");
             snapscroll();
